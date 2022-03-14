6ef7cd0743a7c63406100d9147363b4aa2f2c869
talha0404 <67678204+talha0404@users.noreply.github.com>
2022-03-14 16:00:36 +0300

    Add project files.

---
+++ XamarinArchitecture/XamarinArchitecture/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinArchitecture.Services;
+using XamarinArchitecture.Views;
+
+namespace XamarinArchitecture
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
